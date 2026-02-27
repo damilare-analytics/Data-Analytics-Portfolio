31b6a02c5df51b60980a380d6134a328ea84fade
Dremania007 <damilareorosunsuyi@gmail.com>
2026-02-27 10:28:02 +0100

    Delete Top-10-Product-by-Revenue.sql

--- Top-10-Product-by-Revenue.sql
+++
@@ -1,9 +0,0 @@
-select
-p.name as product_name,
-sum(i.sale_price) as revenue
-from bigquery-public-data.thelook_ecommerce.order_items i
-join bigquery-public-data.thelook_ecommerce.products p
-on i.product_id=p.id
-group by product_name
-order by revenue desc
-limit 10;
